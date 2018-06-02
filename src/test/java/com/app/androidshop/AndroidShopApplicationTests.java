23ccd4d2bb7bdc5d3addb986604b8bfad8d6bdb5
liwei2222 <1820375582@qq.com>
2018-06-02 14:45:17 +0800

    firstcommit

---
+++ src/test/java/com/app/androidshop/AndroidShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.androidshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndroidShopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
